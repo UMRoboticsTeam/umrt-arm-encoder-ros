c0945bfe0c06a75cafa209b131472615cbb77d3e
Toni Odujinrin <todujinrin@gmail.com>
2025-01-06 14:42:47 -0600

    created node

---
+++ include/umrt_arm_encoder_ros/arm_encoder.h
@@ -0,0 +1,65 @@
+#ifndef ARM_ENCODER
+#define ARM_ENCODER
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "sensor_msgs/msg/temperature.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <iostream>
+#include <streambuf> 
+#include <boost/smart_ptr/shared_ptr.hpp> 
+#include <boost/core/null_deleter.hpp> 
+#include <boost/log/core.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/async_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include "../arm-encoder-driver/cpp/encoder_interface.h"
+#include <boost/signals2/slot.hpp>
+
+
+namespace ArmEncoder{
+    class EncoderDiagnosticsBuffer: public std::streambuf{
+        public:
+            EncoderDiagnosticsBuffer(rclcpp::Publisher<std_msgs::msg::String>::SharedPtr diagnostics_publisher); 
+        protected:
+            int override(int c); 
+        private:  
+            void publisher_callback(); 
+            rclcpp::Publisher<std_msgs::msg::String>::SharedPtr m_diagnostics_publisher; 
+            std::string diagnostics_buffer; 
+
+    };
+
+    class Ros2ostream: public std::ostream{
+        public:
+            explicit Ros2ostream(rclcpp::Publisher<std_msgs::msg::String>::SharedPtr diagnostics_publisher);
+
+        private:
+            EncoderDiagnosticsBuffer buffer; 
+    }; 
+
+
+    class ArmEncoderNode: public rclcpp::Node{
+        private:
+            Interface can_interface; 
+            void temperature_handler(uint32_t can_id, double temp); 
+            void angle_handler(uint32_t can_id,double angle, double angular_vel, uint16_t n_rotations); 
+            rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr angle_publisher; 
+            rclcpp::Publisher<sensor_msgs::msg::Temperature>::SharedPtr temp_publisher; 
+            rclcpp::Publisher<std_msgs::msg::String>::SharedPtr diagnostics_publisher; 
+
+        public:
+            ArmEncoderNode(); 
+            void setup_logging(); 
+
+    }; 
+}
+
+
+
+
+
+
+#endif 
---
+++ src/arm_encoder.cpp
@@ -0,0 +1,67 @@
+#include "./arm_encoder.h"
+
+namespace log = boost::log; 
+namespace sources = boost::log::sources; 
+namespace expr = boost::log::expressions; 
+namespace sinks = boost::log::sinks; 
+namespace keywords = boost::log::keywords; 
+
+typedef sinks::asynchronous_sink<sinks::text_ostream_backend> sink_t; 
+
+
+ArmEncoder::EncoderDiagnosticsBuffer::EncoderDiagnosticsBuffer(rclcpp::Publisher<std_msgs::msg::String>::SharedPtr diagnostics_publisher):m_diagnostics_publisher(diagnostics_publisher){}; 
+
+int ArmEncoder::EncoderDiagnosticsBuffer::override(int c){
+    if(c != EOF){
+        if(c == '\n'){
+            publisher_callback(); 
+        }
+        else{
+            diagnostics_buffer+= static_cast<char>(c); 
+        }
+    }
+    return c; 
+}; 
+
+void ArmEncoder::EncoderDiagnosticsBuffer::publisher_callback(){
+    std_msgs::msg::String msg; 
+    msg.data = diagnostics_buffer; 
+    m_diagnostics_publisher->publish(msg); 
+}; 
+
+ArmEncoder::Ros2ostream::Ros2ostream(rclcpp::Publisher<std_msgs::msg::String>::SharedPtr diagnostics_publisher): buffer(diagnostics_publisher),std::ostream(&buffer){}; 
+
+ArmEncoder::ArmEncoderNode::ArmEncoderNode():Node("arm_encoder_node"){
+    this->angle_publisher = this->create_publisher<sensor_msgs::msg::JointState>("encoder_angle",10); 
+    this->temp_publisher = this->create_publisher<sensor_msgs::msg::Temperature>("encoder_temperature",10); 
+    this->diagnostics_publisher = this->create_publisher<std_msgs::msg::String>("encoder_diagnostics",10); 
+    can_interface = Interface(); 
+    can_interface.initialize_channel(); 
+    can_interface.begin_read_loop('a'); 
+    can_interface.begin_read_loop('t'); 
+    can_interface.angle_signal.connect(&angle_handler); 
+    can_interface.temp_signal.connect(&temperature_handler); 
+}; 
+
+void ArmEncoder::ArmEncoderNode::temperature_handler(uint32_t can_id, double temp){
+    sensor_msgs::msg::Temperature msg; 
+    msg.header.frame_id = std::to_string(can_id);
+    msg.temperature = temp; 
+    temp_publisher->publish(msg); 
+}; 
+void ArmEncoder::ArmEncoderNode::angle_handler(uint32_t can_id,double angle, double angular_vel, uint16_t n_rotations ){
+    sensor_msgs::msg::JointState msg; 
+    msg.name[0] = std::to_string(can_id); 
+    msg.position[0] = angle; 
+    msg.velocity[0] = angular_vel; 
+    angle_publisher->publish(msg); 
+}; 
+
+void ArmEncoder::ArmEncoderNode::setup_logging(){
+    boost::shared_ptr<log::core> core = log::core::get(); 
+    auto backend = boost::make_shared<sinks::text_ostream_backend>(); 
+    auto ros_stream = boost::make_shared<Ros2ostream>(this->diagnostics_publisher); 
+    backend->add_stream(ros_stream); 
+    boost::shared_ptr<sink_t> sink(new sink_t(backend)); 
+    core->add_sink(sink); 
+}; 
