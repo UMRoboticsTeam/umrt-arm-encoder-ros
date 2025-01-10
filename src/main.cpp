b39e2d7a955a85bc148935c3f1461bd633c509d6
Toni Odujinrin <todujinrin@gmail.com>
2025-01-10 15:40:53 -0600

    initial code works

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "../include/umrt_arm_encoder_ros/arm_encoder.h"
+
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ArmEncoder::ArmEncoderNode>());
+  rclcpp::shutdown();
+  return 0;
+}
